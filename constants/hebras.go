239d06a2af00929ccfd1306b9f6d6b3a064a7fd1
Miqueas Gimenez <miquigz02@gmail.com>
2024-12-14 19:48:59 -0300

    teaconnections, scrapping

--- constants/hebras.go
+++ constants/hebras.go
@@ -3,6 +3,4 @@ package constants
 const USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (HTML, like Gecko) Chrome/99.0.4844.84 Safari/537.36"
 
 const TEA_BLENDS_URL = "https://www.teablendsforyou.com.ar/te-negro?sort=price_ascending"
-
-//Estos son la mayoria lata de 40g:
-//c.Visit("https://www.teablendsforyou.com.ar/te-negro?sort=price_ascending")
+const TEA_CONNECTION_URL = "https://www.teaconnection.com.ar/collections/te-en-hebras-general?sort_by=price-ascending"
--- services/hebras.go
+++ services/hebras.go
@@ -5,6 +5,7 @@ import (
 	"google.golang.org/appengine/log"
 	"hebras-scrapping/constants"
 	"hebras-scrapping/models"
+	"strings"
 	"sync"
 )
 
@@ -27,7 +28,12 @@ func (hs *HebrasService) ScrapeHebras(urls []string) (teaHebras []models.HebrasT
 		go func(url string) {
 			defer wg.Done()
 			c := colly.NewCollector()
-			hs.scrapeTeaBlends(&teaHebras, c, mutex)
+			if url == constants.TEA_BLENDS_URL {
+				hs.scrapeTeaBlends(&teaHebras, c, mutex)
+			}
+			if url == constants.TEA_CONNECTION_URL {
+				hs.scrapeTeaConnection(&teaHebras, c, mutex)
+			}
 			c.OnRequest(func(r *colly.Request) {
 				r.Headers.Set("User-Agent", constants.USER_AGENT)
 			})
@@ -81,4 +87,45 @@ func (hs *HebrasService) scrapeTeaBlends(teaHebras *[]models.HebrasTea, c *colly
 			mutex.Unlock()
 		}
 	})
+}
+
+// scrapeTeaConnection Realiza el scrapeo en la pagina de TeaConnection
+func (hs *HebrasService) scrapeTeaConnection(teaHebras *[]models.HebrasTea, c *colly.Collector, mutex *sync.Mutex) {
+	c.OnHTML("a.grid-product__link", func(e *colly.HTMLElement) {
+		tea := new(models.HebrasTea)
+		tea.Link = e.Attr("href")
+		tea.Name = e.ChildText("div.grid-product__title p")
+		tea.Img = e.ChildAttr("img.grid-product__image", "src")
+		if tea.Img == "" {
+			tea.Img = e.ChildAttr("img.grid-product__image", "data-srcset")
+			if tea.Img == "" {
+				tea.Img = e.ChildAttr("img.grid-product__image", "srcset")
+			}
+			//tea.Img = e.ChildAttr("img", "src")
+			//TODO: Agregar un else, y hacer un "empty placeholder tea" para .img
+		}
+		tea.Price = e.ChildText("span.variant__price")
+
+		if tea.Price != "" {
+			rawPrice, err := hs.Utils.FormatTeaBlendPrice(tea.Price)
+			if err != nil {
+				log.Infof(nil, "Error al parsear precio: %v", err.Error())
+				return
+			}
+			tea.RawPrice = rawPrice
+		}
+
+		if strings.Contains(tea.Name, "Selecciona tu opción") {
+			tea.Name = strings.Split(tea.Name, "Selecciona tu opción")[0]
+		}
+
+		if tea != nil {
+			tea.From = "TeaConnection"
+			tea.Grams = "40g"
+			mutex.Lock()
+			*teaHebras = append(*teaHebras, *tea)
+			mutex.Unlock()
+		}
+	})
+
 }
